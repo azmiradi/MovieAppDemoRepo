36a731ad65b1a6d77b667a8379fc9a3e797cda7b
azmithabet <azmy-rady@elarabygroup.com>
2023-06-25 23:48:20 +0300

    Add NetModule.kt for dependency injection

---
+++ app/src/main/java/com/azmiradi/movieappdemo/prsentation/di/NetModule.kt
@@ -0,0 +1,56 @@
+package com.azmiradi.movieappdemo.prsentation.di
+
+import com.azmiradi.movieappdemo.BuildConfig
+import com.azmiradi.movieappdemo.data.remote.APIServices
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            if (BuildConfig.DEBUG)
+                this.level = HttpLoggingInterceptor.Level.BODY
+            else
+                this.level = HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHTTPClint(interceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder().apply {
+            this.addInterceptor(interceptor)
+            this.connectTimeout(30, TimeUnit.SECONDS)
+            this.readTimeout(20, TimeUnit.SECONDS)
+            this.writeTimeout(25, TimeUnit.SECONDS)
+        }.build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiServices(retrofit: Retrofit): APIServices {
+        return retrofit.create(APIServices::class.java)
+    }
+}
